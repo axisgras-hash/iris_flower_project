7991bda5db58f697389df9f604aea15e697eae3e
axisgras-hash <axisgras@gmail.com>
2026-02-19 19:47:25 +0530

    Add files via upload

---
+++ app (4).py
@@ -0,0 +1,46 @@
+import streamlit as st
+from sklearn.datasets import load_iris
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pickle
+
+st.title('🪻Iris Flower Classification Project using ML🪻')
+
+
+data = load_iris()
+
+y = data['target']
+X = pd.DataFrame(data['data'],columns = data['feature_names'])
+target_class = data['target_names']
+# target_class
+
+st.sidebar.title('Select Iris Features')
+
+
+user_input = []
+for i in X:
+  min_value = X[i].min()
+  max_value = X[i].max()
+
+  ans = st.sidebar.slider(f'Select value of {i}',min_value,max_value)
+  user_input.append(ans)
+
+
+final_input = [user_input]
+
+with open('iris_model.pkl', 'wb') as f:
+  chatgpt = pickle.load(f)
+
+final_ans = chatgpt.predict(final_input)[0]
+flower_name = target_class[final_ans]
+prob = chatgpt.predict_proba(final_input).ravel()
+
+for i,j in enumerate(prob):
+  flower = target_class[i]
+  st.write(f'Probability of {flower} is : {round(j*100,2)}')
+
+st.success(f'The Final predicted Flower is : {flower_name}')
+
+
